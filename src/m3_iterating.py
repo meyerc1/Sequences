74379c65519f6aee248b9f23bd7c3dee2dab4e6a
meyerc1 <meyerc1@rose-hulman.edu>
2017-12-20 09:23:40 -0500

    All TODO's finished.

--- src/m3_iterating.py
+++ src/m3_iterating.py
@@ -4,23 +4,23 @@ in its most classic form:
   -- Iterate all the way through the sequence, from beginning to end.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Carson Meyer.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    run_test_count_negatives()
-    run_test_count_short_ones()
+    #run_test_count_negatives()
+    #run_test_count_short_ones()
     run_test_draw_circles()
 
 
 def run_test_count_negatives():
     """ Tests the   count_negatives   function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this TEST function.
+    # DONE: 2. Implement this TEST function.
     #   It TESTS the  count_negatives  function defined below.
     #   Include at least ** 2 ** ADDITIONAL tests beyond those we wrote.
     #
@@ -100,15 +100,22 @@ def count_negatives(seq):
       :type seq: list or tuple
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
+    count = 0
+    for k in range(len(seq)):
+
+        if seq[k] < 0:
+            count = count + 1
+
+    return count
 
 
 def run_test_count_short_ones():
     """ Tests the   count_short_ones   function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this TEST function.
+    # DONE: 4. Implement this TEST function.
     #   It TESTS the  count_short_ones  function defined below.
     #   Include at least ** 2 ** ADDITIONAL tests beyond those we wrote.
     #
@@ -183,6 +190,21 @@ def run_test_count_short_ones():
 
     # TO DO 4 (continued):  Add your 2 ADDITIONAL test(s) here:
 
+    #Test 8:
+    expected = 4
+    seq = [[4, 2], ['at', 'boy', 'tea', 'car'], ['juice', 'banana'], [1], [555555]]
+    actual = count_short_ones(seq)
+    print()
+    print('Test 8 expected:', expected)
+    print('       actual:   ', actual)
+
+    #Test 9:
+    expected = 5
+    seq = [['zo'],[2],[1],['ta'],['cr'], [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]]
+    actual = count_short_ones(seq)
+    print()
+    print('Test 9 expected', expected)
+    print('     actual:     ', actual)
 
 def count_short_ones(seq_of_lists):
     """
@@ -205,10 +227,16 @@ def count_short_ones(seq_of_lists):
       :type seq_of_lists: list or tuple (whose items are sequences too)
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
+    count = 0
+    for k in range(len(seq_of_lists)):
 
+        if len(seq_of_lists[k]) < 3:
+            count = count + 1
+
+    return count
 
 def run_test_draw_circles():
     """ Tests the   draw_circles   function. """
@@ -286,9 +314,15 @@ def draw_circles(window, points, radius, color):
       :type color: str
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #          Tests have been written for you (above).
     # ------------------------------------------------------------------
+    for k in range(len(points)):
+        circle = rg.Circle(points[k], radius)
+        circle.fill_color = color
+        circle.attach_to(window)
+        window.render(.05)
+
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
